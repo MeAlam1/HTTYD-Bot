9e5a032e278b6ec306f276bfdab2114b32c3ee5f
Aram <schonhagearam@gmail.com>
2024-02-04 12:23:25 +0100

    Updated Autorole

--- src/components/HTOAD/autorole.js
+++ src/components/HTOAD/autorole.js
@@ -8,15 +8,14 @@ module.exports = {
     const guildId = member.guild.id;
 
     // Define an array of role IDs to assign to the user
-    const autoRoleIds = ['1158395515713626172', '1120099102298996876', '1158393719440031895', '1158394497244336170', '1158394548838477956']; // Add more role IDs as needed
+    const autoRoleIds = ['1158395515713626172', '1120099102298996876', '1158393719440031895', '1158394497244336170', '1158394548838477956'];
 
     // Check if the guild has the GUILD_MEMBERS intent enabled
     if (!client.options.intents.has(GatewayIntentBits.GuildMembers)) {
-      console.error('The GUILD_MEMBERS intent is not enabled. Please enable it in your bot settings.');
+      console.error('I dont have the permission to do this.');
       return;
     }
 
-    // Loop through the role IDs and add each role to the member
     autoRoleIds.forEach((roleId) => {
       const role = member.guild.roles.cache.get(roleId);
       if (role) {
