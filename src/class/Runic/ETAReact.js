3daa43a5ede6345b3eadc9b0ce82ff602b3905b6
Aram <schonhagearam@gmail.com>
2024-02-05 17:53:36 +0100

    Fixed ETAReact.js + organise Start
    I will be Organising the Entire Workspace and fixing everything!!!!!

--- src/class/Runic/ETAReact.js
+++ src/class/Runic/ETAReact.js
@@ -2,15 +2,16 @@ const { ChatInputCommandInteraction, EmbedBuilder } = require('discord.js');
 const RunicETAEmbed = require('../../components/embed/Runic/ETAEmbed.js');
 
 module.exports = client => {
-    function isWelcomeMessage(content) {
-        const keywords = ["ETA", "eta", "ETa","Eta", "eTA", "eTa", "etA"]; 
-        return keywords.some(keyword => content.includes(keyword));
-    }   
+    function isETAMessage(content) {
+        const pattern = /(^|\s)eta[.,!?]?(\s|$)/i; 
+        return pattern.test(content);
+    }
+    
 
     client.on('messageCreate', async (message) => {
         const targetGuildID = '1151497491288690688';
         if (message.guild.id === targetGuildID) { 
-            if (isWelcomeMessage(message.content)) {
+            if (isETAMessage(message.content)) {
                 await message.reply({ embeds: [RunicETAEmbed] });
             }
         }
