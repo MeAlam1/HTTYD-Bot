39816a1915356d29f2899cba0404c2d0aca5330f
Aram <schonhagearam@gmail.com>
2024-02-06 16:40:10 +0100

    Added CreateTicket to Actually create a channel

--- src/components/buttons/CreateTicketButton.js
+++ src/components/buttons/CreateTicketButton.js
@@ -1,11 +1,32 @@
+const { ChannelType, PermissionFlagsBits } = require('discord.js');
+
 module.exports = {
     customId: 'create-ticket-button',
     run: async (client, interaction) => {
+        const categoryId = '1126638959716470886'; 
+
+        interaction.guild.channels.create({
+            name: 'test',
+            type: ChannelType.GuildText,
+            parent: categoryId, 
+            permissionOverwrites: [
+               {
+                 id: interaction.user.id,
+                 allow: [PermissionFlagsBits.ViewChannel],
+                 deny: [PermissionFlagsBits.SendMessages], 
+              },
+              {
+                 id: interaction.guild.roles.everyone,
+                 deny: [PermissionFlagsBits.ViewChannel],
+              },
+            ],
+        }).then(channel => {
+            console.log(`Created new channel ${channel.name}`);
+        }).catch(console.error);
 
         await interaction.reply({
             content: 'The button has been successfully responded!',
             ephemeral: true
         });
-
     }
 };
