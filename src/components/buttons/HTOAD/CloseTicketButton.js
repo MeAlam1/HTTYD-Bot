d658956048849810426f9a8cce765c61d19937c2
Aram <schonhagearam@gmail.com>
2024-02-06 20:00:30 +0100

    Test

--- src/components/buttons/HTOAD/CloseTicketButton.js
+++ src/components/buttons/HTOAD/CloseTicketButton.js
@@ -5,9 +5,9 @@ module.exports = {
     run: async (client, interaction) => {
         const embed = new EmbedBuilder()
             .setColor(0x0099FF)
-            .setTitle('Transcript')
+            .setTitle(`**${interaction.channel.name}**`)
             .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png'})
-            .setDescription(`The Transcript will be named: **${interaction.channel.name}**`)
+            .setDescription(`The title of this Embed will be the name of the Transcript`)
             .setTimestamp()
             .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
 
