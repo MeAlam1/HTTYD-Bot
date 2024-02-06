9a155d87a97b34573c58e0b266cb705eb23041f3
Aram <schonhagearam@gmail.com>
2024-02-06 17:19:40 +0100

    Test

--- src/commands/General/Ticket/CreateTicket.js
+++ src/commands/General/Ticket/CreateTicket.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const HTOAD = ['1120022058601029652']; // How to Own a Dragon
+const allowedServers = ['1120022058601029652']; // How to Own a Dragon
 const allowedRoles = [
     // How to Own a Dragon
     '1120030006626750474' // Owner Role
@@ -13,6 +15,14 @@ module.exports = {
 
         const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
 
+        if (!interaction.guild || !allowedRoles.includes(interaction.guild.id)) {
+            await interaction.reply({
+                content: 'This command is not available in this server.',
+                ephemeral: true
+            });
+            return;
+        }
+
         if (!hasRole) {
             await interaction.reply({
                 content: 'You do not have permission to use this command.',
@@ -21,19 +31,21 @@ module.exports = {
             return;
         }
 
-        await interaction.reply({
-            embeds: [HTOADCreateTicketEmbed],
-            components: [
-                new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('create-ticket-button')
-                            .setEmoji('<a:Toothless_Dance:1204207905365164032>')
-                            .setLabel('Create a Ticket!')
-                            .setStyle(ButtonStyle.Primary)
-                    ),
-            ]
-        });
+        if (interaction.guild && HTOAD.includes(interaction.guild.id)) {
+            await interaction.reply({
+                embeds: [HTOADCreateTicketEmbed],
+                components: [
+                    new ActionRowBuilder()
+                        .addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('create-ticket-button')
+                                .setEmoji('<a:Toothless_Dance:1204207905365164032>')
+                                .setLabel('Create a Ticket!')
+                                .setStyle(ButtonStyle.Primary)
+                        ),
+                ]
+            });
+        }
     }
 };
 
