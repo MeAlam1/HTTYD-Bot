b30beece6cde6cd9f5abcd40950746298fc10b78
Aram <schonhagearam@gmail.com>
2024-02-06 16:53:49 +0100

    ticket

--- src/components/buttons/CreateTicketButton.js
+++ src/components/buttons/CreateTicketButton.js
@@ -1,10 +1,9 @@
-const { ChannelType, PermissionFlagsBits } = require('discord.js');
+const { ChannelType, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     customId: 'create-ticket-button',
     run: async (client, interaction) => {
         const categoryId = '1126638959716470886'; 
-
         const channelName = `ticket-${interaction.user.username.replace(/\s+/g, '-').toLowerCase()}`;
 
         interaction.guild.channels.create({
@@ -12,20 +11,27 @@ module.exports = {
             type: ChannelType.GuildText,
             parent: categoryId, 
             permissionOverwrites: [
-               {
-                 id: interaction.user.id,
-                 allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages], 
-              },
-              {
-                 id: interaction.guild.roles.everyone,
-                 deny: [PermissionFlagsBits.ViewChannel],
-              },
+                {
+                    id: interaction.user.id,
+                    allow: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages], 
+                },
+                {
+                    id: interaction.guild.roles.everyone,
+                    deny: [PermissionFlagsBits.ViewChannel],
+                },
             ],
-        }).catch(console.error);
+        }).then(channel => {
+            interaction.reply({
+                content: `Your ticket has been created! [${channel.name}](https://discord.com/channels/${interaction.guild.id}/${channel.id})`,
+                ephemeral: true
+            });
 
-        await interaction.reply({
-            content: 'The button has been successfully responded!',
-            ephemeral: true
-        });
+            const embed = new EmbedBuilder()
+                .setTitle('Welcome to your ticket!')
+                .setDescription('Please describe your issue, and we will be with you shortly.')
+                .setColor(0x0099FF);
+
+            channel.send({ embeds: [embed] });
+        }).catch(console.error);
     }
 };
