61c638ac1a0a94bd46d68499d2d9e17333291696
Aram <schonhagearam@gmail.com>
2024-02-06 13:43:52 +0100

    Test

--- src/components/embed/HTOAD/CreateTicketEmbed.js
+++ src/components/embed/HTOAD/CreateTicketEmbed.js
@@ -2,9 +2,9 @@ const { EmbedBuilder } = require('discord.js');
 
 const HTOADCreateTicketEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
-    .setTitle('Create a Ticket!')
+    .setTitle('Ticket & Support')
     .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png'})
-    .setDescription('Click the button below to create a ticket!')
+    .setDescription('Click the button below to create a Ticket!')
     .setTimestamp()
     .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
 
