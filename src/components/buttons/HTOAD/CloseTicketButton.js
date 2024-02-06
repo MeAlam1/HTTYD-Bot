caac72d1f9f2d3a0be6cfa73f3ec018332bc70a2
Aram <schonhagearam@gmail.com>
2024-02-06 20:01:51 +0100

    Change name

--- src/components/buttons/HTOAD/CloseTicketButton.js
+++ src/components/buttons/HTOAD/CloseTicketButton.js
@@ -7,7 +7,7 @@ module.exports = {
             .setColor(0x0099FF)
             .setTitle(`**${interaction.channel.name}**`)
             .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png'})
-            .setDescription(`The title of this Embed will be the name of the Transcript`)
+            .setDescription(`The title of this Embed will be the name of the Transcript!`)
             .setTimestamp()
             .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
 
