7d306fc37d2d9671bfe78c4b90ed6c05dee98b36
Aram <schonhagearam@gmail.com>
2024-02-18 14:18:59 +0100

    Updated Embed

--- src/embed/HTOAD/Application/ApplyEmbed.js
+++ src/embed/HTOAD/Application/ApplyEmbed.js
@@ -2,25 +2,23 @@ const { EmbedBuilder } = require('discord.js');
 
 const HTOADApplyEmbed = new EmbedBuilder()
     .setColor(0x0099FF)
-    .setTitle(`Apply`)
+    .setTitle(`Which role would you like to have in our team?`)
     .setURL('https://discord.com/channels/1120022058601029652/1120088926737141923/1120095097061658685')
     .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png'})
     .addFields(
-        { name: `Which role would you like to have in our team?`, value: `
-
-        ***Developer***
-        Discord Bot Coder - The Coders who bring our Bots functionality to life.(Discord.js, Node.js)
-        Website Coder - The Coders who bring our website's functionality to life.(HTML, CSS, JS)
-        Modeler - The individuals who Model and Texture dragons for our mod.
-        Animator - The animators who breathe life into our mod with their dynamic animations.
-        SFX Artist - The Sound Creators, crafting the immersive audio experience of our mod.
-        Concept Artist - The Artists creating stunning visuals for our mod.
-        Builder - The team building intricate structures within the mod.
-        Texture Artist - The contributors focused on item and model textures visible in the mod.
-
-        ***Helper***
-        Tester - The Testers ensuring the mod's quality and stability.
-        Translator - The translators expanding our mod's reach by adapting it into various languages.
+        { name: `***Developer***`, value: `
+        **Discord Bot Coder** - The Coders who bring our Bots functionality to life.(Discord.js, Node.js)
+        **Website Coder** - The Coders who bring our website's functionality to life.(HTML, CSS, JS)
+        **Modeler** - The individuals who Model and Texture dragons for our mod.
+        **Animator** - The animators who breathe life into our mod with their dynamic animations.
+        **SFX Artist** - The Sound Creators, crafting the immersive audio experience of our mod.
+        **Concept Artist** - The Artists creating stunning visuals for our mod.
+        **Builder** - The team building intricate structures within the mod.
+        **Texture Artist** - The contributors focused on item and model textures visible in the mod.
+    ` },
+        { name: `***Helper***`, value: `
+        **Tester** - The Testers ensuring the mod's quality and stability.
+        **Translator** - The translators expanding our mod's reach by adapting it into various languages.
     ` },
     )
     .setTimestamp()
