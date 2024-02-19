693296322bf5b6cfb5307153c71a98bd1cb1aca2
Aram <schonhagearam@gmail.com>
2024-02-19 14:39:40 +0100

    Made /apply public

--- src/commands/HTOAD/Important/Apply.js
+++ src/commands/HTOAD/Important/Apply.js
@@ -1,28 +1,13 @@
 const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 const HTOAD = ['1120022058601029652']; // How to Own a Dragon
-const allowedRoles = [
-    // How to Own a Dragon
-    '1120030006626750474', // Owner Role
-    '1133420066277437490' // Lead Dev Role
-];
 const HTOADApplyEmbed = require('../../../embed/HTOAD/Application/ApplyEmbed.js');
 
 
 module.exports = {
     structure: new SlashCommandBuilder()
         .setName('apply')
-        .setDescription('HTOAD Admin Only(for now).'),
+        .setDescription('Execute this command to Apply for How to Own a Dragon!'),
     run: async (client, interaction, args) => {
-
-        const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
-
-        if (!hasRole) {
-            await interaction.reply({
-                content: 'You do not have permission to use this command.',
-                ephemeral: true
-            });
-            return;
-        }
 
         if (interaction.guild && HTOAD.includes(interaction.guild.id)) {
             await interaction.reply({
