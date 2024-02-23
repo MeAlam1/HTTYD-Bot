382708645f71e73300ec9870a7043e86ac2e348c
Aram <schonhagearam@gmail.com>
2024-02-23 18:17:22 +0100

    Commented some Events

--- src/events/Runic/ETA/ETA.js
+++ src/events/Runic/ETA/ETA.js
@@ -1,3 +1,9 @@
+/**Description: 
+ * This Event will filter all messages sent in Runic Public server.
+ * It Checks them for ETA.
+ * If ETA is found it will send a message in the channel.
+ */
+
 const RunicETAEmbed = require('../../../embed/Runic/Information/ETAEmbed.js');
 
 module.exports = {
@@ -8,6 +14,12 @@ module.exports = {
         const message = args[0]; 
 
         function isETAMessage(content) {
+            /**
+             * The list of words that the message will be checked for.
+             * The pattern will check for the word "eta" in the message.
+             * will also flag with Spaces, Punctuation and Special Characters.
+             * Case insensitive.
+             */
             const pattern = /([.,!?~*@#$%^&()_\-{}+=:;"'</?|`\s]|^)eta([.,!?~*@#$%^&()_\-{}+=:;"'</?|`\s]|$)/i;
             return pattern.test(content);
         }
@@ -15,6 +27,8 @@ module.exports = {
         const Runic = '1151497491288690688'; // Runic Isles Public Server
 
         if (message.guild && message.guild.id === Runic && isETAMessage(message.content)) {
+            
+            // Send an embed in the channel.
             await message.reply({ embeds: [RunicETAEmbed] });
         }
     }
--- src/events/Runic/Moderation/AntiSpam.js
+++ src/events/Runic/Moderation/AntiSpam.js
@@ -1,3 +1,12 @@
+/**Description:
+ * This event will filter all messages sent in Runic Public server.
+ * It Checks them for Spam. [@everyone or @here ]
+ * If @everyone or @here are found it will delete the message.
+ * Send a Message in the Automod Channel.
+ * Timeout the User for 7 days.
+ * The Owner, Management, Moderator, Team Leader, Mod developer and Bots roles are allowed to use @everyone or @here in the server.
+ */
+
 module.exports = {
     event: 'messageCreate',
     once: false,
@@ -6,17 +15,18 @@ module.exports = {
         const message = args[0];
 
         function isSpam(content) {
+            // The list of words that the message will be checked for.
             const pattern = ['@everyone', '@here'];
             return pattern.some(spamWord => content.includes(spamWord));
         }
 
         const allowedRoles = [
-            '1151500042843201576', // Owner
-            '1189510610556301332', // Management
-            '1203303940364439573', // Moderator
-            '1202369001779499129', // Team Leader
-            '1151500178331807896', // Mod developer
-            '1151571613440020631'  // Bots
+            '1151500042843201576', // Server Owner Role
+            '1189510610556301332', // Server Management Role
+            '1203303940364439573', // Server Moderator Role
+            '1202369001779499129', // Team Leader Role
+            '1151500178331807896', // Mod developer Role
+            '1151571613440020631'  // Bots Role
         ];
 
         const Runic = '1151497491288690688'; // Runic Public server
@@ -26,14 +36,20 @@ module.exports = {
 
             if (!hasAllowedRole) {
                 try {
+                    
+                    // Delete the Spam message.
                     await message.delete();
 
                     const logChannelId = '1151645114146488390'; // Runic Public log channel
                     const logChannel = await client.channels.fetch(logChannelId);
 
+                    // Message sent in the log channel.
                     await logChannel.send({ content: `${message.author.tag} has been timed out for using @ everyone or @ here inappropriately.` });
 
-                    const timeoutDuration = 7 * 24 * 60 * 60 * 1000; // 7 days
+                    // Timeout duration in milliseconds.(7 Days)
+                    const timeoutDuration = 7 * 24 * 60 * 60 * 1000;
+                    
+                    // Message in Audit Log.
                     await message.member.timeout(timeoutDuration, 'Using @  everyone or @ here inappropriately.');
                 } catch (error) {
                     console.error('Error trying to delete a spam message or timeout the user: ', error);
