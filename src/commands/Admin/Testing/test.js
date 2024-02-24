db9d394546f3d74358d68cf148aaed30b71bf05f
Aram <schonhagearam@gmail.com>
2024-02-24 16:07:19 +0100

    Changed the Brackets

--- src/commands/Admin/Testing/test.js
+++ src/commands/Admin/Testing/test.js
@@ -1,39 +1,39 @@
 // Testing The new: AntiSpam Logging Embed
 
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const HTOAD = ['1120022058601029652']; // How to Own a Dragon
+const { SlashCommandBuilder, EmbedBuilder } = require(`discord.js`);
+const HTOAD = [`1120022058601029652`]; // How to Own a Dragon
 const allowedRoles = [
-    '1120030006626750474', // How to Own a Dragon Owner Role
-    '1133420066277437490', // How to Own a Dragon Lead Dev Role
-    '1140629154748956813'  // How to Own a Dragon Coder Role
+    `1120030006626750474`, // How to Own a Dragon Owner Role
+    `1133420066277437490`, // How to Own a Dragon Lead Dev Role
+    `1140629154748956813`  // How to Own a Dragon Coder Role
 ];
 
 const TestEmbed = new EmbedBuilder()
     .setColor(0xbf020f)
-    .setTitle('[User IGN]') // IGN User that did the Punishment
-    .setURL('https://discord.js.org/') // URL to User
-    .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png'})
-    .setDescription('Usage of @everyone and @here in the server!')
-    .setThumbnail('https://i.imgur.com/AfFp7pu.png') // Profile Picture of User
+    .setTitle(`[User IGN]`) // IGN User that did the Punishment
+    .setURL(`https://discord.js.org/`) // URL to User
+    .setAuthor({ name: `How to Own a Dragon`, iconURL: `https://i.imgur.com/VTwEDBO.png`})
+    .setDescription(`Usage of @everyone and @here in the server!`)
+    .setThumbnail(`https://i.imgur.com/AfFp7pu.png`) // Profile Picture of User
     .addFields(
-        { name: 'The User Ping:', value: '***User:*** [ping]⠀⠀⠀⠀', inline: true }, // The Ping of the User
-        { name: 'The User ID:', value: '***ID:*** [message]⠀⠀⠀', inline: true  }, // The ID of the User
-        { name: 'The Message ID:', value: '***ID:*** [message]⠀⠀⠀', inline: true  }, // The Message ID
+        { name: `The User Ping:`, value: `***User:*** [ping]⠀⠀⠀⠀`, inline: true }, // The Ping of the User 
+        { name: `The User ID:`, value: `***ID:*** [message]⠀⠀⠀`, inline: true  }, // The ID of the User
+        { name: `The Message ID:`, value: `***ID:*** [message]⠀⠀⠀`, inline: true  }, // The Message ID
     )
     .addFields(
-        { name: 'Message Content:', value: '***Message:*** [Message]' } // The Message that got deleted
+        { name: `Message Content:`, value: `***Message:*** [Message]` } // The Message that got deleted
     )
     .setTimestamp()
-    .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
+    .setFooter({ text: `How to Own a Dragon Coder Team`, iconURL: `https://i.imgur.com/VTwEDBO.png` });
 
 module.exports = {
     structure: new SlashCommandBuilder()
-        .setName('test')
-        .setDescription('Admin Only!!!'),
+        .setName(`test`)
+        .setDescription(`Admin Only!!!`),
     run: async (client, interaction, args) => {
         if (!interaction.guild || !HTOAD.includes(interaction.guild.id)) {
             await interaction.reply({
-                content: 'This command is not available in this server.',
+                content: `This command is not available in this server.`,
                 ephemeral: true
             });
             return;
@@ -43,7 +43,7 @@ module.exports = {
         
         if (!hasRole) {
             await interaction.reply({
-                content: 'You do not have permission to use this command.',
+                content: `You do not have permission to use this command.`,
                 ephemeral: true
             });
             return;
@@ -51,6 +51,7 @@ module.exports = {
 
             await interaction.reply({
                 embeds: [TestEmbed]
+                
             });
     }
 };
