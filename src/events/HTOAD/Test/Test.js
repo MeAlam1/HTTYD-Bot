53b84af19418e6c4c1688be114146eb748c45957
Aram <schonhagearam@gmail.com>
2024-02-24 16:37:34 +0100

    Changed the Embed Again

--- src/events/HTOAD/Test/Test.js
+++ src/events/HTOAD/Test/Test.js
@@ -35,10 +35,9 @@ module.exports = {
             .addFields(
                 { name: 'The User:', value: `<@${message.author.id}>⠀⠀⠀⠀`, inline: true }, // The Ping of the User
                 { name: 'The User ID:', value: `${message.author.id}⠀⠀⠀⠀`, inline: true }, // The ID of the User
-                { name: '\u200B', value: '\u200B' }
             )
             .addFields(
-                { name: 'Message Content:', value: `${message.content}⠀⠀⠀⠀`, inline: true }, // The Message that was sent
+                { name: 'Message Content:', value: `${message.content}⠀⠀⠀⠀`}, // The Message that was sent
                 { name: 'The Message ID:', value: `${message.id}⠀⠀⠀⠀`, inline: true  }, // The Message ID
             )
             .setTimestamp()
