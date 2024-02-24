513eab176260ac3b7ed0f36c93b6633923fcffd5
Aram <schonhagearam@gmail.com>
2024-02-24 16:42:15 +0100

    Added the embed to AntiSpam

--- src/events/HTOAD/Moderation/AntiSpam.js
+++ src/events/HTOAD/Moderation/AntiSpam.js
@@ -7,6 +7,8 @@
  * The Owner, Lead Dev and Bot roles are allowed to use @everyone or @here in the server.
  */
 
+const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
     event: 'messageCreate',
     once: false,
@@ -34,6 +36,23 @@ module.exports = {
             if (!hasAllowedRole) {
                 try {
 
+                    const AntiSpamLog = new EmbedBuilder()
+                        .setColor(0xbf020f)
+                        .setTitle(`${message.author.tag}`) // User that sent the message
+                        .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png' })
+                        .setDescription('Usage of @everyone and @here in the server!')
+                        .setThumbnail(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 })) // Profile Picture of User
+                        .addFields(
+                            { name: 'The User:', value: `<@${message.author.id}>⠀⠀⠀⠀`, inline: true }, // The Ping of the User
+                            { name: 'The User ID:', value: `${message.author.id}⠀⠀⠀⠀`, inline: true }, // The ID of the User
+                        )
+                        .addFields(
+                            { name: 'Message Content:', value: `${message.content}⠀⠀⠀⠀`}, // The Message that was sent
+                            { name: 'The Message ID:', value: `${message.id}⠀⠀⠀⠀`, inline: true  }, // The Message ID
+                        )
+                        .setTimestamp()
+                        .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
+
                     // Delete the Spam message.
                     await message.delete();
 
@@ -41,7 +60,7 @@ module.exports = {
                     const logChannel = await client.channels.fetch(logChannelId);
 
                     // Message sent in the log channel.
-                    await logChannel.send({ content: `${message.author.tag} has been timed out for using @ everyone or @ here inappropriately.` });
+                    await logChannel.send({ embed: AntiSpamLog });
 
                     // Timeout duration in milliseconds.(7 Days)
                     const timeoutDuration = 7 * 24 * 60 * 60 * 1000; 
