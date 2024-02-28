26aebd114c99c81687e985c1adf1d72dd3a24c0c
Aram <schonhagearam@gmail.com>
2024-02-28 16:27:24 +0100

    Updated the Query's

--- src/commands/Admin/Testing/deleteSchema.js
+++ src/commands/Admin/Testing/deleteSchema.js
@@ -6,6 +6,18 @@ module.exports = {
         .setName(`delete-chema`)
         .setDescription(`esting a Schema`),
     run: async (client, interaction) => {
+        const allowedRoles = [
+            '1120030006626750474', // How to Own a Dragon Owner Role
+            '1133420066277437490', // How to Own a Dragon Lead Dev Role
+            '1140629154748956813'  // How to Own a Dragon Coder Role
+        ];
+
+        const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
+
+        if (!hasRole) {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return;
+        }
 
         const data = await testSchema.find(); 
 
