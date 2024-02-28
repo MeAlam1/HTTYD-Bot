98f8cd8c467fb59396c03d82abdf19502c2c7525
Aram <schonhagearam@gmail.com>
2024-02-28 16:20:05 +0100

    Updated the Database Query accesebility

--- src/commands/Admin/Testing/testSchema.js
+++ src/commands/Admin/Testing/testSchema.js
@@ -7,6 +7,18 @@ module.exports = {
         .setDescription(`Testing the Schema`)
         .addStringOption(option => option.setName(`schema-input`).setDescription(`text to save.`).setRequired(true)),
     run: async (client, interaction) => {
+        const allowedRoles = [
+            '1120030006626750474', // How to Own a Dragon Owner Role
+            '1133420066277437490', // How to Own a Dragon Lead Dev Role
+            '1140629154748956813'  // How to Own a Dragon Coder Role
+        ];
+
+        const hasRole = interaction.member.roles.cache.some(role => allowedRoleIds.includes(role.id));
+
+        if (!hasRole) {
+            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+            return;
+        }
 
         const { options } = interaction;
         const string = options.getString(`schema-input`);
@@ -16,6 +28,5 @@ module.exports = {
         });
 
         await interaction.reply('I saved the data');
-
     }
 };
