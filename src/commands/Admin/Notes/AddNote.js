a09d1492f4897e7cefdf4f6c5011df5ed035dbd2
Aram <schonhagearam@gmail.com>
2024-03-03 18:38:21 +0100

    Fixed the Embed Builder

--- src/commands/Admin/Notes/AddNote.js
+++ src/commands/Admin/Notes/AddNote.js
@@ -29,65 +29,66 @@ module.exports = {
         .addBooleanOption(option => 
             option.setName('dm')
                 .setDescription('DM the user?')),
-    run: async (client, interaction) => {
-        const allowedRoles = [
-            '1120030006626750474', // How to Own a Dragon Owner Role
-            '1133420066277437490', // How to Own a Dragon Lead Dev Role
-        ];
+        run: async (client, interaction) => {
+            const allowedRoles = [
+                '1120030006626750474', // How to Own a Dragon Owner Role
+                '1133420066277437490', // How to Own a Dragon Lead Dev Role
+            ];
+                
+            const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
+                
+            if (!hasRole) {
+                await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+                return;
+            }
+                
+            const { options } = interaction;
+                
+            const attachmentOption = options.getAttachment('attachment');
+            let attachments = [];
+            if (attachmentOption) {
+                attachments.push(attachmentOption.url);
+            }
+                
+            const userOption = options.getUser('user');
+            const noteDocument = await NoteSchema.create({
+                guild: interaction.guild.id,
+                moderator: interaction.user.id,
+                user: userOption.id,
+                note: options.getString('note'),
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                isHidden: false,
+                type: options.getString('type') || 'general',
+                status: options.getString('status') || 'closed',
+                attachments: attachments,
+                visibility: 'public',
+                dmNotification: options.getBoolean('dm') || false,
+            });
 
-        const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
-
-        if (!hasRole) {
-            await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
-            return;
-        }
-
-        const { options } = interaction;
-
-        const attachmentOption = options.getAttachment('attachment');
-        let attachments = [];
-        if (attachmentOption) {
-            attachments.push(attachmentOption.url);
-        }
-
-        await NoteSchema.create({
-            guild: interaction.guild.id,
-            moderator: interaction.user.id,
-            user: options.getUser('user').id,
-            note: options.getString('note'),
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            isHidden: false,
-            type: options.getString('type') || 'general',
-            status: options.getString('status') || 'closed',
-            attachments: attachments,
-            visibility: 'public',
-            dmNotification: options.getBoolean('dm') || false,
-        });
-
-        const noteEmbed = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(`A note for ${NoteSchema.user} has been created! `) 
-        .setURL(`https://discord.com/users/${NoteSchema.user}`)
-        .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png' })
-        .setThumbnail(options.getUser('user').displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-        .addFields(
-            { name: 'Moderator', value: `<@${NoteSchema.moderator}>`, inline: true },
-            { name: 'User', value: `<@${NoteSchema.user}>`, inline: true },
-            { name: 'Note Type', value: NoteSchema.type, inline: true },
-            { name: 'Status', value: NoteSchema.status, inline: true },
-            { name: 'visibility', value: NoteSchema.visibility, inline: true },
-            { name: 'DM Notification', value: NoteSchema.dmNotification, inline: true },
-            { name: 'Created At', value: NoteSchema.createdAt, inline: true },
-            { name: 'Note', value: NoteSchema.note },
-        )
-        .setTimestamp()
-        .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
-
-    if (attachments.length > 0) {
-        noteEmbed.setImage(attachments[0]); 
+            const noteEmbed = new EmbedBuilder()
+                .setColor('#0099ff')
+                .setTitle(`A note for ${userOption.tag} has been created!`)
+                .setURL(`https://discord.com/users/${userOption.id}`)
+                .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/VTwEDBO.png' })
+                .setThumbnail(userOption.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+                .addFields(
+                    { name: 'Moderator', value: `<@${interaction.user.id}>`, inline: true },
+                    { name: 'User', value: `<@${userOption.id}>`, inline: true },
+                    { name: 'Note Type', value: noteDocument.type, inline: true },
+                    { name: 'Status', value: noteDocument.status, inline: true },
+                    { name: 'Visibility', value: noteDocument.visibility, inline: true },
+                    { name: 'DM Notification', value: noteDocument.dmNotification ? 'Yes' : 'No', inline: true },
+                    { name: 'Created At', value: noteDocument.createdAt.toISOString(), inline: true },
+                    { name: 'Note', value: noteDocument.note },
+                )
+                .setTimestamp()
+                .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/VTwEDBO.png' });
+                
+            if (attachments.length > 0) {
+                noteEmbed.setImage(attachments[0]); // Assuming the URL is correct
+            }
+                
+        await interaction.reply({ embeds: [noteEmbed] });
     }
-
-    await interaction.reply({ embeds: [noteEmbed] });
-    }
-};
+};                
