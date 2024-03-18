884769f4dcb266302a29bc22289815786a71eaf8
Aram <schonhagearam@gmail.com>
2024-03-18 19:47:56 +0100

    Reverted Back to old Autoupdate

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,51 +1,31 @@
+# DO NOT TOUCH THIS FILE!
+
+# Description: This script is used to automatically update the bot for the host.
+
 #!/bin/bash
 
-# Description: This script is used to automatically update the bot for the host and reboot the PC after 24 hours.
-
-# Function to restart the bot
-restart_bot() {
-  echo "Restarting the bot..."
-  pkill node
-  /home/aram/.nvm/versions/node/v19.9.0/bin/npm run dev &
-  echo "Bot restarted."
-}
-
-# Initial bot restart
+# Restart the bot once before entering the update loop
 echo "Initial bot restart..."
-restart_bot
-
-# Record start time
-start_time=$(date +%s)
+pkill node
+npm run dev &
+echo "Bot restarted."
 
 while true; do
-  # Calculate elapsed time in hours
-  current_time=$(date +%s)
-  elapsed_time=$((current_time - start_time))
-  hours_elapsed=$((elapsed_time / 3600))
-
-  # Check if 24 hours have passed
-  if [ "$hours_elapsed" -ge 24 ]; then
-    echo "24 hours have passed. Initiating reboot..."
-    # Use 'sudo' for reboot command; may require password input or NOPASSWD configuration in /etc/sudoers
-    sudo reboot
-    break # Exit the script
-  fi
-
   # Check if the Github has been updated
   git fetch origin
 
-  # Check for any new changes
+  # Check if there are any new changes
   if ! git diff --quiet origin/main; then
     echo "New changes detected! Pulling updates..."
     # Pull the latest changes
     git pull
 
+    echo "Restarting the bot..."
     # Restart the bot
-    restart_bot
+    pkill node
+    npm run dev &
+    echo "Bot restarted."
   else
     echo "No new changes detected."
   fi
-
-  # Wait for 5 seconds before the next iteration
-  sleep 5
 done
