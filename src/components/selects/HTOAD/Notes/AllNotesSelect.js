1325799676439f22042e1eb2a5932fa9860ae18c
Aram <schonhagearam@gmail.com>
2024-03-22 15:47:40 +0100

    Changed the Button to a Select Menu

--- src/components/selects/HTOAD/Notes/AllNotesSelect.js
+++ src/components/selects/HTOAD/Notes/AllNotesSelect.js
@@ -4,7 +4,7 @@
  * src\components\selects\HTOAD\Notes\ServerNotesSelect.js
  */
 
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 const NoteSchema = require('../../../../schemas/Notes/NotesSchema.js');
 
 module.exports = {
@@ -54,11 +54,11 @@ module.exports = {
         await interaction.reply({ embeds: [noteDetailsEmbed], components: [
             new ActionRowBuilder()
             .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('edit-note-button')
-                    .setLabel('Edit Note')
-                    .setStyle(ButtonStyle.Primary)
-                    .setEmoji('✏️')
+                new StringSelectMenuBuilder()
+                    .setCustomId('edit-note-select')
+                    .setPlaceholder('What part of the node do you want to edit?')
+                    .addOptions(),
+
             ),
 
         ]});
