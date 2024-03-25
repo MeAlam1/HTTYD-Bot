3d67b8978dda4a8338d865e46affec08a067c53a
Aram <schonhagearam@gmail.com>
2024-03-25 21:26:39 +0100

    Made some buttons a variable

--- src/components/buttons/HTOAD/Info/ChannelsButton.js
+++ src/components/buttons/HTOAD/Info/ChannelsButton.js
@@ -1,23 +1,21 @@
-/**Description:
- * This is an button to display the Channels!
- * src\embed\HTOAD\Information\ChannelsEmbed.js
- * src\commands\Admin\Information\Information.js
- */
-
 const ChannelsEmbed = require('../../../../embed/HTOAD/Information/ChannelsEmbed.js');
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     customId: 'htoad-channels-button',
     run: async (client, interaction) => {
-        await interaction.reply({ embeds: [ChannelsEmbed], components: [
-            new ActionRowBuilder
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('htoad-archive-button')
-                        .setLabel('Archive')
-                        .setStyle(ButtonStyle.Primary)
-                )
-        ], ephemeral: true});
+        const archiveRow = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('htoad-archive-button')
+                    .setLabel('Archive')
+                    .setStyle(ButtonStyle.Primary)
+            );
+
+        await interaction.reply({ 
+            embeds: [ChannelsEmbed], 
+            components: [archiveRow],
+            ephemeral: true
+        });
     }
 };
