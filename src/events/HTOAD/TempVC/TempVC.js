58e42668c198f2433403dc8a4a58bc716c1bf3d6
Aram <schonhagearam@gmail.com>
2024-04-30 13:25:47 +0200

    Updated Feature

--- src/events/HTOAD/TempVC/TempVC.js
+++ src/events/HTOAD/TempVC/TempVC.js
@@ -24,7 +24,7 @@ module.exports = {
     run: async (client, oldState, newState) => {
         if (newState.channelId === VcMakerChannel && oldState.channelId !== newState.channelId) {
             const guild = newState.guild;
-            const vcName = `${newState.member.user.username}'s channel ${Date.now()}`;
+            const vcName = `${newState.member.user.username}'s channel`;
             const category = newState.channel.parent;
             try {
                 const newVC = await guild.channels.create(vcName, {
