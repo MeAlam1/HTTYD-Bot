3216db949f51deb7a583be58d2f2e97a368433d7
Aram <schonhagearam@gmail.com>
2024-05-04 15:47:40 +0200

    Updated FAQ Embed

--- src/embed/HTOAD/Information/FaqEmbed2.js
+++ src/embed/HTOAD/Information/FaqEmbed2.js
@@ -13,48 +13,49 @@ const FaqEmbed2 = new EmbedBuilder()
     .setAuthor({ name: 'How to Own a Dragon', iconURL: 'https://i.imgur.com/gSjyLDH.png'})
     .setImage('https://imgur.com/xEKQnvq.png')
     .setDescription(`
-    *What is this?*
-    This mod, How to Own a Dragon, is a Minecraft Forge modification inspired by the How to Train Your Dragon series. 
-    ***Note: This is a mod for Minecraft Java Edition, not Bedrock.
-    It cannot be played on consoles. 
-    For mobile, an emulator is required.***
+*What is this?*
+This mod, How to Own a Dragon, is a Minecraft Forge modification inspired by the How to Train Your Dragon series. 
+***Note: This is a mod for Minecraft Java Edition, not Bedrock.
+It cannot be played on consoles. 
+For mobile, an emulator is required.***
 
-    *Who's behind the mod?*
-    <@578557512983511057> and <@397416283835990016> are the Co-Owners. The Lead Developers are <@751818438879608843> and <@798910386907906068>. We also have numerous <@&1152958350095556708> and <@&1156838077344526417> assisting us.
+*Who's behind the mod?*
+@pushedtester21 and @me_alam are the Co-Owners. The Lead Developers are @agent.echo and @another_toaster. We also have numerous @HelperGrunt and @Screaming Dev assisting us.
 
-    *How long has the mod been in development?*
-    Development of How to Own a Dragon began on June 17th, 2023.
+*How long has the mod been in development?*
+Development of How to Own a Dragon began on June 17th, 2023.
 
-    *Release date of the mod?*
-    Currently, there's no set date for the mod's first stable release, but the first alpha version was released on July 17th, 2023.
+*Release date of the mod?*
+Currently, there's no set date for the mod's first stable release, but the first alpha version was released on July 17th, 2023.
 
-    *Are there beta testers?*
-    Indeed, we have testers who rigorously check each version for bugs. Each tester is assigned the <@&1138158375650218086> role.
+*Are there beta testers?*
+Indeed, we have testers who rigorously check each version for bugs. Each tester is assigned the @Hideous ZippleBug role.
 
-    *Can I contribute to the mod?*
-    Absolutely! There are various ways to contribute: modelling, texturing, animating, translating, testing, or building. For more information or to apply, refer to <#1136677313967300730> or <#1158305505660383232>. We welcome all levels of experience.
-    <#1136677313967300730> = Tester, Translator.
-    <#1158305505660383232> = Modeler, Animator, Texturer, Coder, Builder.
+*Can I contribute to the mod?*
+Absolutely! There are various ways to contribute: modelling, texturing, animating, translating, testing, or building. For more information or to apply, refer to #helper-application or #dev-application. We welcome all levels of experience.
+#helper-application = Tester, Translator.
+#dev-application = Modeler, Animator, Texturer, Coder, Builder.
 
-    *How can I support the mod financially?*
-    Yes, while the mod remains free, you can support us financially through our [Patreon](https://www.patreon.com/htoad) or [Ko-Fi](https://ko-fi.com/htoad). Funds will be used for various purposes, including running our server.
+*How can I support the mod financially?*
+Yes, while the mod remains free, you can support us financially through our [Patreon](https://www.patreon.com/htoad) or [Ko-Fi](https://ko-fi.com/htoad). Funds will be used for various purposes, including running our server.
 
-    *Where can I find more information about the mod?*
-    For detailed information, visit our Wiki on [Miraheze!](https://howtoownadragon.miraheze.org/wiki/Main_Page)
+*Where can I find more information about the mod?*
+For detailed information, visit our Wiki on [Miraheze!](https://howtoownadragon.miraheze.org/wiki/Main_Page)
 
-    *How to report a bug?*
-    To report bugs or translation issues, use <#1176135677247750215> or submit them on [The Github](https://github.com/MeAlam1/How-to-Own-a-Dragon/issues).
+*How to report a bug?*
+To report bugs or translation issues, use #issues or submit them on [The Github](https://github.com/MeAlam1/How-to-Own-a-Dragon/issues).
 
-    *Can I help with translations?*
-    Certainly! You can translate the mod into any language via [The Crowdin](https://crowdin.com/project/how-to-own-a-dragon). Although HTOAD has specific languages targeted for translation, if yours isn't listed, open a ticket, and we'll sort it out. For recognition of your translation work, see <#1136677313967300730> or <#1126637161400242236>.
+*Can I help with translations?*
+Certainly! You can translate the mod into any language via [The Crowdin](https://crowdin.com/project/how-to-own-a-dragon). Although HTOAD has specific languages targeted for translation, if yours isn't listed, open a ticket, and we'll sort it out. For recognition of your translation work, see #ticket/#guide or 
+use /apply in any channel.
 
-    *Where to find the mod?*
-    Find us on our [Curseforge page!](https://curseforge.com/minecraft/mc-mods/how-to-own-a-dragon)
+*Where to find the mod?*
+Find us on our [Curseforge page!](https://curseforge.com/minecraft/mc-mods/how-to-own-a-dragon)
 
-    *Trouble downloading the mod?*
-    If you encounter download issues, first check <#1150041146828013568>. If the problem persists, create a new thread in <#1129499336137519175>.
+*Trouble downloading the mod?*
+If you encounter download issues, first check #"Problems with downloading an Alpha". If the problem persists, create a new thread in #qna.
 
-    Last updated: <t:1706185176>
+Last updated: <t:1714830347>
 `)
     .setTimestamp()
     .setFooter({ text: 'How to Own a Dragon Coder Team', iconURL: 'https://i.imgur.com/gSjyLDH.png' });
