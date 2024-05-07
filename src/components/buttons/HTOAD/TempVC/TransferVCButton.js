17ef115cd613d0b055725709729dd2b4ee5a41d4
Aram <schonhagearam@gmail.com>
2024-05-07 13:57:57 +0200

    Updated some File names

---
+++ src/components/buttons/HTOAD/TempVC/TransferVCButton.js
@@ -0,0 +1,38 @@
+/**Description:
+ * This button is used to transfer a Temp VC!
+ * src\events\HTOAD\TempVC\TempVC.js
+ */
+
+const { ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, PermissionFlagsBits } = require('discord.js');
+    
+module.exports = {
+    customId: 'htoad-transfer-button',
+    run: async (client, interaction) => {
+
+        const modal = new ModalBuilder()
+        .setTitle('Transfer VC')
+        .setCustomId('transfer-vc-modal')
+        .addComponents(
+            new ActionRowBuilder()
+                .addComponents(
+                    new TextInputBuilder()
+                        .setLabel('Please give the username or ID of the user you want to transfer the VC to!')
+                        .setCustomId('transfer-vc')
+                        .setPlaceholder('Type the username or ID here!(ID works best)')
+                        .setStyle(TextInputStyle.Short)
+                        .setRequired(true)
+                )
+        );
+
+        const memberPermissions = interaction.channel.permissionsFor(interaction.member).has(PermissionFlagsBits.ManageChannels);
+        if (!memberPermissions) {
+            await interaction.reply({
+                content: 'You do not have the required permissions to use this command.',
+                ephemeral: true
+            });
+            return;
+        }
+        await interaction.showModal(modal);
+
+    }
+};
---
+++ src/components/modals/HTOAD/TempVC/TransferVCModal.js
@@ -0,0 +1,31 @@
+/**
+ * Description: 
+ * This Modal is used to transfer a VC to a user.
+ * src\components\buttons\HTOAD\TempVC\LimitButton.js
+ */
+
+let member;
+
+module.exports = {
+    customId: 'transfer-vc-modal',
+    run: async (client, interaction) => {
+        const inputValue = interaction.fields.getTextInputValue('transfer-vc');
+        
+        if (interaction.guild.members.cache.has(inputValue)) {
+            member = interaction.guild.members.cache.get(inputValue);
+        } else {
+            member = interaction.guild.members.cache.find(m => m.user.username === inputValue);
+        }
+
+        if (!member) {
+            console.error(`No member found with ID or username: ${inputValue}`);
+            interaction.reply({ content: `No member found with ID or username: ${inputValue}`, ephemeral: true });
+        } else {
+            
+            await interaction.channel.permissionOverwrites.create(member, {
+                manageChannels: true,
+              })
+            await interaction.reply({ content: `The VC has been transferred to ${member}.`, ephemeral: true });
+        }
+    }
+};
