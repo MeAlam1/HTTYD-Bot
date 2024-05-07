7cc2125ea85168545a90621bed344a8fcff72688
Aram <schonhagearam@gmail.com>
2024-05-07 21:53:24 +0200

    Created a leetmap

--- src/functions/profanityFilter.js
+++ src/functions/profanityFilter.js
@@ -1,3 +1,23 @@
 const Profanease = require('profanease');
+
+const leetMap = {
+    '4': 'a', '@': 'a', '3': 'e', '1': 'i', '!': 'i', '0': 'o', '7': 't', '5': 's', '8': 'b', '$': 's', '#': 'h'
+};
+
+function replaceLeet(message) {
+    return message.split('').map(char => leetMap[char.toLowerCase()] || char).join('');
+}
+
 const profanityFilter = new Profanease({ placeHolder: 'x'});
-module.exports = profanityFilter;
+
+function check(message) {
+    const cleanedMessage = replaceLeet(message);
+    return profanityFilter.check(cleanedMessage);
+}
+
+function clean(message) {
+    const cleanedMessage = replaceLeet(message);
+    return profanityFilter.clean(cleanedMessage);
+}
+
+module.exports = { check, clean };
