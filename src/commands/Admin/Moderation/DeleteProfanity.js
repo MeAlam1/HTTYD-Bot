a0ff2bec7aed2db9a5813bd05f459847130d764e
Aram <schonhagearam@gmail.com>
2024-05-07 21:21:16 +0200

    Updated the Filter

---
+++ src/commands/Admin/Moderation/DeleteProfanity.js
@@ -0,0 +1,58 @@
+/**Servers:
+ * How to Own a Dragon
+ */
+
+/**Description:
+ * This command is used to Remove Profanity from the database.
+ * ADMIN ONLY COMMAND
+ */
+
+const profanityFilter = require('../../../functions/profanityFilter.js');
+const { SlashCommandBuilder } = require('discord.js');
+
+
+const allowedServers = [
+    '1120022058601029652', // How to Own a Dragon Server
+];
+
+const allowedRoles = [
+    // How to Own a Dragon
+    '1120030006626750474', // Owner Role
+    '1133420066277437490', // Lead Dev Role
+    '1161418815440166943', // Moderator Role
+];
+
+module.exports = {
+    structure: new SlashCommandBuilder()
+        .setName('removecurse')
+        .setDescription('Remove a Profanity Word from the database.')
+        .addStringOption(option =>
+            option.setName('profanity')
+                .setDescription('Profanity to remove to the database.')
+                .setRequired(true)),
+        run: async (client, interaction) => {
+
+            if (!allowedServers.includes(interaction.guild.id)) {
+                await interaction.reply({ content: 'This command is not available in this server.', ephemeral: true });
+                return;
+            }
+
+            const hasRole = interaction.member.roles.cache.some(role => allowedRoles.includes(role.id));
+                
+            if (!hasRole) {
+                await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+                return;
+            }
+
+            const Profanity = interaction.options.getString('profanity');
+
+            try {
+                profanityFilter.removeWords([Profanity]);
+                await interaction.reply({ content: 'Profanity word has been removed from the list.', ephemeral: true });
+            } catch (error) {
+                await interaction.reply({ content: 'An error occurred while adding the profanity word.', ephemeral: true });
+                return;
+            }
+
+        }
+    }            
