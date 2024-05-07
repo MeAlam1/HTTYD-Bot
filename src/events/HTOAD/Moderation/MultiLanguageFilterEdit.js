b1ca7bd43ea3091f156bb12e5520c2fe247b0e1e
Aram <schonhagearam@gmail.com>
2024-05-08 00:07:12 +0200

    Updated the Bot for some logging

--- src/events/HTOAD/Moderation/MultiLanguageFilterEdit.js
+++ src/events/HTOAD/Moderation/MultiLanguageFilterEdit.js
@@ -27,7 +27,13 @@ module.exports = {
         if (containIgnoreWord) return;
         if (containsspamWord || profanityFilter.check(message.content) || profanityFilter.check(message.content.toLowerCase()) || profanityFilter.check(message.content.toUpperCase()) || profanityFilter.check(message.content.charAt(0).toUpperCase() + message.content.slice(1)) || profanityFilter.check(message.content.charAt(0).toLowerCase() + message.content.slice(1))) {
             if (allowedServers.includes(message.guild.id)) {
+                const cleanMessage = profanityFilter.clean(message.content);
                 await message.delete();
+
+                if (message.guild.id === '1120022058601029652') {
+                    const channel = message.guild.channels.cache.get('1168633539676344490');
+                    channel.send(`**<@${message.author.id}>** said: ${message.content}\n\nCleaned message: ${cleanMessage}`);
+                }
             }
         }
     }
