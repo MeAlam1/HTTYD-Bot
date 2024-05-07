092a26bac821d6d51476539e6139009bb177ec88
Aram <schonhagearam@gmail.com>
2024-05-08 00:39:30 +0200

    Update

--- src/functions/profanityFilter.js
+++ src/functions/profanityFilter.js
@@ -5,6 +5,9 @@ const leetMap = {
 };
 
 function replaceLeet(message) {
+    if (!message) {
+        return '';
+    }
     return message.split('').map(char => leetMap[char.toLowerCase()] || char).join('');
 }
 
